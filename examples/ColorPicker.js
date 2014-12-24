878330d32219ef95e55293827ac618a1db0f006c
Justin Manley <manleyjster@gmail.com>
2014-12-24 00:07:41 -0500

    Show color swatch in toolbar icon under examples/popup.html.

--- examples/ColorPicker.js
+++ examples/ColorPicker.js
@@ -1,19 +1,5 @@
 L.ColorPicker = L.ToolbarHandler.extend({
 	options: {
-		toolbarIcon: { 
-			className: 'leaflet-color-picker', 
-			html: '<span class="fa fa-eyedropper"></span>' 
-		}
-	},
-
-	initialize: function(map, options) {
-		L.setOptions(this, options);
-		L.ToolbarHandler.prototype.initialize.call(this, map, options);
-	}
-});
-
-L.Edit.Color = L.ToolbarHandler.extend({
-	options: {
 		toolbarIcon: { className: 'leaflet-color-swatch' }
 	},
 
@@ -29,5 +15,21 @@ L.Edit.Color = L.ToolbarHandler.extend({
 
 		this._shape.setStyle({ color: this.options.color });
 		this.disable();
+	},
+
+	_createIcon: function(toolbar, container, args) {
+		var colorSwatch = L.DomUtil.create('div'),
+			width, height;
+
+		L.ToolbarHandler.prototype._createIcon.call(this, toolbar, container, args);
+
+		L.extend(colorSwatch.style, {
+			backgroundColor: this.options.color,
+			width: L.DomUtil.getStyle(this._link, 'width'),
+			height: L.DomUtil.getStyle(this._link, 'height'),
+			border: '3px solid ' + L.DomUtil.getStyle(this._link, 'backgroundColor')
+		});
+
+		this._link.appendChild(colorSwatch);
 	}
 });
