914c4c759b8828f7c71b08b0b8ba1da2f91a86a1
Sebastian B. Knapp <news@young-workers.de>
2017-06-01 18:08:19 +0200

    fix indentation

--- test/src/ReusableControlSpec.js
+++ test/src/ReusableControlSpec.js
@@ -28,14 +28,14 @@ describe("Reusable Controls", function() {
         }),
         click = function (el){
             var ev = document.createEvent("MouseEvent");
-                 ev.initMouseEvent(
+            ev.initMouseEvent(
                      "click",
                      true /* bubble */, true /* cancelable */,
                      window, null,
                      0, 0, 0, 0, /* coordinates */
                      false, false, false, false, /* modifier keys */
                      0 /*left*/, null
-                );
+            );
             el.dispatchEvent(ev);
         };
 
